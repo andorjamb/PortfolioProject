188c7a7ac07e5e0aaccc5ce6bb9dbf1b5a8e80b1
Anna Petelin <andorjamb@protonmail.com>
2022-04-30 15:58:24 +1000

    blog updates, adding jquery script source to blog page

--- index.js
+++ index.js
@@ -2,5 +2,25 @@ window.onload = initAll;
 
 function initAll() {
   console.log("script is loaded and operational!");
-  alert("knock knock");
+}
+
+function rollover() {
+  for (var i = 0; i < document.images.length; i++) {
+    if (document.images[i].parentNode.tagName == "A") {
+      setupRollover(document.images[i]);
+    }
+  }
+}
+
+function setupRollover(thisImage) {
+  thisImage.outImage = new Image();
+  thisImage.outImage.src = thisImage.src;
+  thisImage.onmouseout = function () {
+    this.src = this.outImage.src;
+  };
+  thisImage.overImage = new Image();
+  thisImage.overImage.src = "images/" + thisImage.id + "_on.gif";
+  thisImage.onmouseover = function () {
+    this.src = this.overImage.src;
+  };
 }
