0e93588efec1c4eb5a71f4c31a73f291f062b786
Anna Petelin <andorjamb@protonmail.com>
2022-04-30 23:20:43 +1000

    updates to image manipulation post

--- index.js
+++ index.js
@@ -2,25 +2,24 @@ window.onload = initAll;
 
 function initAll() {
   console.log("script is loaded and operational!");
-}
 
-function rollover() {
   for (var i = 0; i < document.images.length; i++) {
     if (document.images[i].parentNode.tagName == "A") {
+      //this is a link tag - the tag name should be in capitals
       setupRollover(document.images[i]);
     }
   }
-}
 
-function setupRollover(thisImage) {
-  thisImage.outImage = new Image();
-  thisImage.outImage.src = thisImage.src;
-  thisImage.onmouseout = function () {
-    this.src = this.outImage.src;
-  };
-  thisImage.overImage = new Image();
-  thisImage.overImage.src = "images/" + thisImage.id + "_on.gif";
-  thisImage.onmouseover = function () {
-    this.src = this.overImage.src;
-  };
+  function setupRollover(thisImage) {
+    thisImage.outImage = new Image();
+    thisImage.outImage.src = thisImage.src;
+    thisImage.onmouseout = function () {
+      this.src = this.outImage.src;
+    };
+    thisImage.overImage = new Image();
+    thisImage.overImage.src = "./assets/images/" + thisImage.id + "_on.png";
+    thisImage.onmouseover = function () {
+      this.src = this.overImage.src;
+    };
+  }
 }
