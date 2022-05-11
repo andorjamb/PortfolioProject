3381b7acd73f4ef04116a7b840fe95115535dd9c
Anna Petelin <andorjamb@protonmail.com>
2022-05-11 18:22:07 +1000

    changes to blog javascript, element maniuplation

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ window.onload = initAll;
 
 function initAll() {
   console.log("script is loaded and operational!");
-//for all images on the page that are also links:
+  //for all images on the page that are also links:
   for (var i = 0; i < document.images.length; i++) {
     if (document.images[i].parentNode.tagName == "A") {
       //this is a link tag - the tag name should be in capitals
       setupRollover(document.images[i]);
     }
   }
-
+//this function's code is from the Negrino/Smith textbook
   function setupRollover(thisImage) {
     thisImage.outImage = new Image();
     thisImage.outImage.src = thisImage.src;
@@ -17,9 +17,13 @@ function initAll() {
       this.src = this.outImage.src;
     };
     thisImage.overImage = new Image();
+    //note to self: you need to upload a suitable image for this function
     thisImage.overImage.src = "./assets/images/" + thisImage.id + "_on.png";
     thisImage.onmouseover = function () {
       this.src = this.overImage.src;
     };
   }
+  //function hideElement(thisImage) {
+
+  //}
 }
